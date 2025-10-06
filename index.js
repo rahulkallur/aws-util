5d468750bcb419e564bdc94a50638dba97648473
Rahul Kallur <52211405+rahulkallur@users.noreply.github.com>
2025-10-06 16:35:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+// list.js
+const { S3Client, ListBucketsCommand, GetBucketLocationCommand } = require("@aws-sdk/client-s3");
+
+const client = new S3Client({
+  region:"ap-south-1"
+});
+
+async function main() {
+  let command = new ListBucketsCommand()
+
+  let data = await client.send(command)
+  console.log("Data:",data)
+}
+
+main()
